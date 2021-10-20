d06c3e7d53db25e67e8ad6b09ac92966b817c839
Brandon De Leon <brandondeleon.615@gmail.com>
2021-10-20 15:24:54 -0500

    added funtion to query animals by status

--- animals/__init__.py
+++ animals/__init__.py
@@ -1,2 +1,3 @@
 from .request import get_all_animals, get_single_animal, create_animal
 from .request import delete_animal, update_animal, get_animals_by_location_id
+from .request import get_animals_by_status
--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animals_by_location_id
-from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animals_by_location_id, get_animals_by_status
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee, get_employees_by_location_id
 from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
 from customers import get_single_customer, get_all_customers, create_customer, delete_customer, update_customer, get_customers_by_email
 
@@ -117,6 +117,10 @@ class HandleRequests(BaseHTTPRequestHandler):
                 response = get_customers_by_email(value)
             elif key == "location_id" and resource == "animals":
                 response = get_animals_by_location_id(value)
+            elif key == "location_id" and resource == "employees":
+                response = get_employees_by_location_id(value)
+            elif key == "status" and resource == "animals":
+                response = get_animals_by_status(value)
 
         self.wfile.write(response.encode())
 
